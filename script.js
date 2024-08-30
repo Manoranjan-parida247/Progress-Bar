61aba427bbdb3334de17f328c263d25968cbfa03
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2024-08-30 19:48:48 +0530

    adding all fiel

---
+++ script.js
@@ -0,0 +1,50 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+});
+
+prev.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+});
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    // Calculate the progress percentage
+    const progressPercent = ((currentActive - 1) / (circles.length - 1)) * 100;
+    progress.style.background = `linear-gradient(to right, #3498db ${progressPercent}%, #d3d3d3 0%)`;
+
+    // Enable or disable buttons based on currentActive
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
